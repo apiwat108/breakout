faca060715fd8d8f5ac94af58e252e7a16f09aa9
apiwat108 <89001155+apiwat108@users.noreply.github.com>
2023-11-09 06:31:46 +0700

    Update config.py on BREAKOUT_SYMBOLS for 9-11-23

---
+++ config.py
@@ -0,0 +1,18 @@
+API_KEY = 'PKR4A0NILIB6VO3IEYB1'
+SECRET_KEY = '6CMrs8wisLktlhBsR45SC3LEoHgEei6C1MHOAzmi'
+API_URL = 'https://paper-api.alpaca.markets'
+
+HEADERS = {
+    'APCA-API-KEY-ID': API_KEY,
+    'APCA-API-SECRET-KEY': SECRET_KEY
+}
+
+DB_FILE = '/Users/apiwat/Documents/FULLSTACK-APP/app.db'
+BARS_URL = 'https://data.alpaca.markets/v2'
+
+DB_HOST = 'localhost' #https://dev.to/balt1794/postgresql-port-5432-already-in-use-1pcf
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'app'
+
+BREAKOUT_SYMBOLS = ['LTH', 'AYX', 'ROVR', 'SNOW', 'DDOG', 'BMRN', 'FVRR', 'MDB', 'COUR', 'DUST']
