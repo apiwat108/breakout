1a1bd7ccbcb96951223918a9d2e0b4a13a3cbfc8
apiwat108 <89001155+apiwat108@users.noreply.github.com>
2023-12-17 04:23:24 +0700

    Update till 15 Dec

---
+++ config.py
@@ -0,0 +1,18 @@
+API_KEY = 'PKR4A0NILIB6VO3IEYB1'
+SECRET_KEY = '6CMrs8wisLktlhBsR45SC3LEoHgEei6C1MHOAzmi'
+API_URL = 'https://paper-api.alpaca.markets'
+
+HEADERS = {
+    'APCA-API-KEY-ID': API_KEY,
+    'APCA-API-SECRET-KEY': SECRET_KEY
+}
+
+DB_FILE = '/Users/apiwat/Documents/FULLSTACK-APP/app.db'
+BARS_URL = 'https://data.alpaca.markets/v2'
+
+DB_HOST = 'localhost' #https://dev.to/balt1794/postgresql-port-5432-already-in-use-1pcf
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'app'
+
+BREAKOUT_SYMBOLS = ['UNFI', 'VFS', 'FULC', 'OSPN', 'VRCA', 'SAVA', 'NMIH', 'MOMO', 'NETI', 'FCX', 'MBI', 'ACHR', 'VRNA', 'AMBC', 'ALLE', 'ASPN', 'HRL', 'AMRN', 'PHR', 'CHGG', 'CPB', 'GBNH', 'BRP', 'CBD', 'SGEN', 'CTM', 'ATO', 'GKOS', 'PCVX', 'DCI', 'MRK', 'PRQR', 'BLDP', 'INSM', 'ASND', 'LUMN', 'DLTR', 'GEL', 'TUP', 'DEI', 'GLPI', 'GME', 'HIW', 'PLBY', 'IMVT', 'THM', 'AAT', 'BITF', 'PGRE', 'LILM', 'HTLD', 'SVV', 'IREN', 'GOLD', 'BRBR', 'ALEC', 'AGR', 'AMPL', 'VFC', 'FIGS', 'TRMB']
