88a2d1e7c00757ffe43c2bd9c66b6b899ba8a460
apiwat108 <89001155+apiwat108@users.noreply.github.com>
2024-03-07 18:30:45 +0700

    Update config till 7 Mar.py

--- config.py
+++ config.py
@@ -15,5 +15,5 @@ DB_USER = 'postgres'
 DB_PASS = 'password'
 DB_NAME = 'app'
 
-BREAKOUT_SYMBOLS = ['PBH', 'VMD', 'LAB', 'BHVN', 'BGC', 'COHR', 'MGNX', 'FCEL', 'FMC', 'NSC', 'PTGX', 'THC', 'TREX', 'UGI', 'VSTO', 'BALL', 'APG', 'APAM', 'CMS', 'CAVA', 'EW', 'JNPR', 'EHC', 'VTEX', 'SEM', 'AGIO', 'FULC', 'SVV']
-BB_SYMBOLS = ['SERA', 'SUN', 'THC', 'CLH', 'BAH', 'GATX', 'MPC', 'WEBL', 'WOR', 'XLC', 'BFH', 'DOCN', 'BASE', 'DIS', 'BAH', 'CVLT', 'AMG', 'CG', 'FTNT']
+BREAKOUT_SYMBOLS = ['NOK', 'PBH', 'VMD', 'LAB', 'BHVN', 'BGC', 'COHR', 'MGNX', 'FMC', 'NSC', 'PTGX', 'THC', 'TREX', 'UGI', 'VSTO', 'BALL', 'APG', 'APAM', 'CMS', 'CAVA', 'EW', 'JNPR', 'EHC', 'VTEX', 'SEM', 'AGIO', 'FULC', 'SVV']
+BB_SYMBOLS = ['L', 'EW', 'EHC', 'GSK', 'CP', 'PDI', 'RTX', 'ARMK', 'AIV', 'CDP', 'CSWC', 'HL', 'IGR', 'BKR', 'AMCR', 'ERIC', 'SERA', 'SUN', 'THC', 'CLH', 'BAH', 'GATX', 'MPC', 'WEBL', 'WOR', 'XLC', 'BFH', 'DOCN', 'BASE', 'DIS', 'BAH', 'CVLT', 'AMG', 'CG', 'FTNT']
