ffcd2d5c005effe836be40aadca668331b1f2f7c
apiwat108 <89001155+apiwat108@users.noreply.github.com>
2024-03-08 21:18:53 +0700

    Update config double BAH.py

--- config.py
+++ config.py
@@ -16,4 +16,4 @@ DB_PASS = 'password'
 DB_NAME = 'app'
 
 BREAKOUT_SYMBOLS = ['NOK', 'PBH', 'VMD', 'LAB', 'BHVN', 'BGC', 'COHR', 'MGNX', 'FMC', 'NSC', 'PTGX', 'THC', 'TREX', 'UGI', 'VSTO', 'BALL', 'APG', 'APAM', 'CMS', 'CAVA', 'EW', 'JNPR', 'EHC', 'VTEX', 'SEM', 'AGIO', 'FULC', 'SVV']
-BB_SYMBOLS = ['L', 'EW', 'EHC', 'GSK', 'CP', 'PDI', 'RTX', 'ARMK', 'AIV', 'CDP', 'CSWC', 'HL', 'IGR', 'BKR', 'AMCR', 'ERIC', 'SERA', 'SUN', 'THC', 'CLH', 'BAH', 'GATX', 'MPC', 'WEBL', 'WOR', 'XLC', 'BFH', 'DOCN', 'BASE', 'DIS', 'BAH', 'CVLT', 'AMG', 'CG', 'FTNT']
+BB_SYMBOLS = ['L', 'EW', 'EHC', 'GSK', 'CP', 'PDI', 'RTX', 'ARMK', 'AIV', 'CDP', 'CSWC', 'HL', 'IGR', 'BKR', 'AMCR', 'ERIC', 'SERA', 'SUN', 'THC', 'CLH', 'GATX', 'MPC', 'WEBL', 'WOR', 'XLC', 'BFH', 'DOCN', 'BASE', 'DIS', 'BAH', 'CVLT', 'AMG', 'CG', 'FTNT']
