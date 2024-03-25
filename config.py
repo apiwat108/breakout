b8f829b8162dbc7e766ec79419438076b937b888
apiwat108 <89001155+apiwat108@users.noreply.github.com>
2024-03-25 16:16:43 +0700

    Update for 25 Mar

---
+++ config.py
@@ -0,0 +1,20 @@
+API_KEY = 'PK1578DRKR3CHSG28IEW'
+SECRET_KEY = 'arRvpMcSnXTed5bhc4FhMzl8kyX1TBepqJKzOC1N'
+API_URL = 'https://paper-api.alpaca.markets'
+
+HEADERS = {
+    'APCA-API-KEY-ID': API_KEY,
+    'APCA-API-SECRET-KEY': SECRET_KEY
+}
+
+DB_FILE = '/Users/apiwat/Documents/FULLSTACK-APP/app.db'
+BARS_URL = 'https://data.alpaca.markets/v2'
+
+DB_HOST = 'localhost' #https://dev.to/balt1794/postgresql-port-5432-already-in-use-1pcf
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'app'
+
+BREAKOUT_SYMBOLS = ['PHAT', 'DAVE', 'IDN', 'GHM', 'SMLP', 'ACB', 'PHAT', 'WULF', 'FRGE', 'TKO', 'YOU', 'STLD', 'RTX', 'QNST', 'PFG', 'PBH', 'VMD', 'LAB', 'BGC', 'FMC', 'THC', 'TREX', 'VSTO', 'APG', 'CAVA', 'EW', 'SEM']
+
+BB_SYMBOLS = ['BALL', 'PEG', 'AR', 'AVTR', 'RRC', 'SHAK', 'FRPT', 'TRIP', 'CRK', 'MWA', 'HRB', 'LUNR', 'JHG', 'GFL', 'GEN', 'LC', 'SLDP', 'TV', 'WOR', 'AAON', 'TNL', 'ATXS', 'SPRY', 'SDHC', 'SFM', 'NNOX', 'COOP', 'TRIP', 'GM', 'CAL', 'CMC', 'FGEN', 'BDTX', 'NBBK', 'DOG', 'CIM', 'STNE', 'SWI', 'ARI', 'NG', 'L', 'EW', 'EHC', 'GSK', 'CP', 'PDI', 'RTX', 'ARMK', 'AIV', 'CDP', 'CSWC', 'HL', 'IGR', 'BKR', 'AMCR', 'ERIC', 'SERA', 'SUN', 'THC', 'CLH', 'GATX', 'MPC', 'WEBL', 'WOR', 'XLC', 'BFH', 'DOCN', 'BASE', 'DIS', 'BAH', 'CVLT', 'AMG', 'CG', 'FTNT']
