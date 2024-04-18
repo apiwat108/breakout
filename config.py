b7d0021a61f92f3fb54ab4a63e970d79fda5f0a6
apiwat108 <89001155+apiwat108@users.noreply.github.com>
2024-04-18 20:36:56 +0700

    Update with Breakout_1-3 Criteria

---
+++ config.py
@@ -0,0 +1,18 @@
+API_KEY = 'PK1578DRKR3CHSG28IEW'
+SECRET_KEY = 'arRvpMcSnXTed5bhc4FhMzl8kyX1TBepqJKzOC1N'
+API_URL = 'https://paper-api.alpaca.markets'
+
+HEADERS = {
+    'APCA-API-KEY-ID': API_KEY,
+    'APCA-API-SECRET-KEY': SECRET_KEY
+}
+
+DB_FILE = '/Users/apiwat/Documents/FULLSTACK-APP/app.db'
+BARS_URL = 'https://data.alpaca.markets/v2'
+
+DB_HOST = 'localhost' #https://dev.to/balt1794/postgresql-port-5432-already-in-use-1pcf
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'app'
+
+BREAKOUT_SYMBOLS = ['CUTR', 'CMT', 'ZUO', 'SOXS', 'INVO', 'DFLI', 'SPXU', 'VFF', 'VERI', 'VET', 'KDP', 'IGC', 'CAG', 'KHC', 'NPWR', 'SAND', 'AGR', 'WEN', 'DYNT', 'ELME', 'CADL', 'CACO', 'PROK', 'BSGM', 'IMAX', 'HUSA', 'NATL', 'MP', 'GSL', 'ALXO', 'GCI', 'RANI', 'RENT', 'SBSW', 'RVMD', 'DXD', 'TWM', 'DOG', 'SPDN', 'TZA', 'SRTY', 'TECS']
