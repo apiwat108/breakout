f3a290794a78259a635e8b6cbe75427815310439
apiwat108 <89001155+apiwat108@users.noreply.github.com>
2024-04-26 17:25:09 +0700

    Delete low volatility symbol

---
+++ config.py
@@ -0,0 +1,18 @@
+API_KEY = 'PK1578DRKR3CHSG28IEW'
+SECRET_KEY = 'arRvpMcSnXTed5bhc4FhMzl8kyX1TBepqJKzOC1N'
+API_URL = 'https://paper-api.alpaca.markets'
+
+HEADERS = {
+    'APCA-API-KEY-ID': API_KEY,
+    'APCA-API-SECRET-KEY': SECRET_KEY
+}
+
+DB_FILE = '/Users/apiwat/Documents/FULLSTACK-APP/app.db'
+BARS_URL = 'https://data.alpaca.markets/v2'
+
+DB_HOST = 'localhost' #https://dev.to/balt1794/postgresql-port-5432-already-in-use-1pcf
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'app'
+
+BREAKOUT_SYMBOLS = ['CRK', 'PROK', 'GPRK', 'DOG', 'WEN', 'MP', 'KMI', 'CGC', 'POR', 'SBSW', 'HUSA', 'NPWR', 'GCI', 'SRTY', 'TZA', 'FOX', 'NATL', 'CMT', 'GSL', 'KNTK', 'AMPY', 'HRL', 'ALXO', 'KDP', 'DXD', 'UGI', 'TECS', 'BALY', 'RENT', 'FNGR', 'AGR', 'PARA', 'NN', 'PRMW', 'TWM', 'JNUG', 'RVMD', 'KHC', 'VIRT', 'VFF', 'ELME', 'PUMP', 'WISH', 'DFLI', 'CTRA', 'INVO', 'CADL', 'JBLU', 'SPXU', 'SPDN', 'ZUO', 'MAG', 'GLNG', 'NUGT', 'CAG', 'CUTR', 'FOXA', 'IMAX', 'ORGN', 'MRT', 'GDXU', 'SOXS', 'HUMA', 'SAND']
