ad6330367541c9b44472a7eaa77dfc1af0b387cc
apiwat108 <89001155+apiwat108@users.noreply.github.com>
2024-05-03 18:22:09 +0700

    Update for May 3

---
+++ config.py
@@ -0,0 +1,22 @@
+API_KEY = 'PK1578DRKR3CHSG28IEW'
+SECRET_KEY = 'arRvpMcSnXTed5bhc4FhMzl8kyX1TBepqJKzOC1N'
+API_URL = 'https://paper-api.alpaca.markets'
+
+HEADERS = {
+    'APCA-API-KEY-ID': API_KEY,
+    'APCA-API-SECRET-KEY': SECRET_KEY
+}
+
+DB_FILE = '/Users/apiwat/Documents/FULLSTACK-APP/app.db'
+BARS_URL = 'https://data.alpaca.markets/v2'
+
+DB_HOST = 'localhost' #https://dev.to/balt1794/postgresql-port-5432-already-in-use-1pcf
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'app'
+
+BREAKOUT_SYMBOLS_DX0 = ['LFST', 'PTCT', 'LADR', 'NWL', 'NLY', 'TNL', 'ASAN', 'SHOO', 'RBBN', 'TRIP', 'ST', 'MRUS', 'NYT', 'CGON', 'ORC', 'AGEN', 'BEPC', 'CFLT', 'JMIA', 'SLI', 'UNIT', 'CDP', 'MCRB', 'NAT', 'CYH', 'RIOT', 'DAR', 'CORZ', 'ETWO', 'RXT', 'VICI', 'BFH', 'DX', 'LICN', 'GME', 'SHCR', 'IONQ', 'NEO', 'HIW', 'ABR', 'KC', 'SEER', 'SYF', 'ANGI', 'HCP', 'DBI', 'JOBY', 'AB', 'BEP']
+
+BREAKOUT_SYMBOLS_DR1 = ['AY', 'LFST', 'LADR', 'NWL', 'PTCT', 'MOBX', 'MO', 'PFS', 'AMBP', 'CUTR', 'EQT', 'AGBA', 'MTTR', 'LC', 'WEAT', 'LILM', 'FITB', 'RBBN', 'ALGM', 'QGEN', 'ZUO', 'AGEN', 'BEPC', 'RILY', 'CYH', 'UGI', 'POR', 'POET', 'CMRE', 'DFLI', 'ABVC', 'BFH', 'IRDM', 'FTDR', 'OZK', 'SNAP', 'ZIM', 'FLO', 'HUMA', 'ENB', 'KC', 'CGEM', 'SEER', 'SYF', 'HCP', 'HWC', 'DAN', 'JOBY', 'ANVS', 'BEP', 'SMPL', 'KRG']
+
+BREAKOUT_SYMBOLS_DX1 = ['CGC', 'LAUR', 'FSM', 'KGC', 'PWFL', 'TTI', 'CPG', 'SNDL', 'BTE', 'OII', 'FXN', 'SAND', 'TH', 'SMTC', 'GSM', 'MGY', 'EXK', 'AMRK', 'LU', 'STOK', 'AUR', 'IE', 'REAL', 'CRON', 'TALO', 'EH', 'ACVA', 'MUX', 'CVE', 'EC', 'OUST', 'DOC', 'DBC', 'SG', 'AG', 'AA', 'MAG', 'OPK', 'VET', 'AES', 'MUR', 'HLX']
