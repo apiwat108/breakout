8395b503f5986a0804f578d698cdbae6b7b2da73
apiwat108 <89001155+apiwat108@users.noreply.github.com>
2024-05-16 18:33:44 +0700

    Update for May 16

---
+++ config.py
@@ -0,0 +1,19 @@
+API_KEY = 'PKEQJTZ7QZME52SF9DC8'
+SECRET_KEY = 'OVTOPLtaHdaceplanKmtkrS4xthqQzr4eHooCzlV'
+API_URL = 'https://paper-api.alpaca.markets'
+
+HEADERS = {
+    'APCA-API-KEY-ID': API_KEY,
+    'APCA-API-SECRET-KEY': SECRET_KEY
+}
+
+DB_FILE = '/Users/apiwat/Documents/FULLSTACK-APP/app.db'
+BARS_URL = 'https://data.alpaca.markets/v2'
+
+DB_HOST = 'localhost' #https://dev.to/balt1794/postgresql-port-5432-already-in-use-1pcf
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'app'
+
+BREAKOUT_SYMBOLS_5MX0_AB = ['INO', 'HUT', 'NPWR', 'CTLP', 'GORO', 'GRNT', 'STOK', 'TTI', 'GOEV', 'KAR', 'MIR', 'TLS', 'CARG', 'GETY', 'IQ', 'NINE', 'CNK', 'HMY', 'AA', 'MPW', 'DM', 'OPK', 'HLN', 'EXK', 'HUYA', 'IMAX', 'POWW', 'ORGO', 'PGEN', 'GM', 'DNN', 'EQX', 'USM', 'DCGO', 'OCSL', 'ORLA', 'GTX', 'HOOD', 'NOV', 'SG', 'GERN', 'CX', 'HLMN', 'NKLA', 'EGY', 'HROW', 'GROY']
+BREAKOUT_SYMBOLS_5MX0_BB = ['AR', 'CNX', 'ALGM', 'AKR', 'MAG', 'KRG', 'DK', 'CXAI', 'EMX', 'GROY', 'EGY', 'DOUG', 'HAL', 'EGO']
