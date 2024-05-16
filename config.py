eb272ae09c0b193547974549701d42327c7a8f32
apiwat108 <89001155+apiwat108@users.noreply.github.com>
2024-05-16 20:53:11 +0700

    Update config.py

--- config.py
+++ config.py
@@ -15,5 +15,5 @@ DB_USER = 'postgres'
 DB_PASS = 'password'
 DB_NAME = 'app'
 
-BREAKOUT_SYMBOLS_5MX0_AB = ['INO', 'HUT', 'NPWR', 'CTLP', 'GORO', 'GRNT', 'STOK', 'TTI', 'GOEV', 'KAR', 'MIR', 'TLS', 'CARG', 'GETY', 'IQ', 'NINE', 'CNK', 'HMY', 'AA', 'MPW', 'DM', 'OPK', 'HLN', 'EXK', 'HUYA', 'IMAX', 'POWW', 'ORGO', 'PGEN', 'GM', 'DNN', 'EQX', 'USM', 'DCGO', 'OCSL', 'ORLA', 'GTX', 'HOOD', 'NOV', 'SG', 'GERN', 'CX', 'HLMN', 'NKLA', 'EGY', 'HROW', 'GROY']
-BREAKOUT_SYMBOLS_5MX0_BB = ['AR', 'CNX', 'ALGM', 'AKR', 'MAG', 'KRG', 'DK', 'CXAI', 'EMX', 'GROY', 'EGY', 'DOUG', 'HAL', 'EGO']
+BREAKOUT_SYMBOLS_5MX0_AB = ['INO', 'HUT', 'NPWR', 'CTLP', 'GORO', 'GRNT', 'STOK', 'TTI', 'GOEV', 'KAR', 'MIR', 'TLS', 'CARG', 'GETY', 'IQ', 'NINE', 'CNK', 'AA', 'MPW', 'DM', 'OPK', 'HLN', 'EXK', 'HUYA', 'IMAX', 'POWW', 'ORGO', 'PGEN', 'GM', 'DNN', 'USM', 'DCGO', 'OCSL', 'ORLA', 'GTX', 'HOOD', 'NOV', 'SG', 'GERN', 'CX', 'HLMN', 'NKLA', 'EGY', 'HROW', 'GROY']
+BREAKOUT_SYMBOLS_5MX0_BB = ['AR', 'CNX', 'ALGM', 'AKR', 'MAG', 'KRG', 'DK', 'CXAI', 'EMX', 'GROY', 'EGY', 'DOUG', 'HAL', 'EGO', 'EQX']
