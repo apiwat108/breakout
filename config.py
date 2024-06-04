95455e2face67649b15e882d4ca8f83c7c236d32
apiwat108 <89001155+apiwat108@users.noreply.github.com>
2024-06-04 16:52:09 +0700

    Update for Jun 4

---
+++ config.py
@@ -0,0 +1,20 @@
+API_KEY = 'PKEQJTZ7QZME52SF9DC8'
+SECRET_KEY = 'OVTOPLtaHdaceplanKmtkrS4xthqQzr4eHooCzlV'
+API_URL = 'https://paper-api.alpaca.markets'
+
+HEADERS = {
+    'APCA-API-KEY-ID': API_KEY,
+    'APCA-API-SECRET-KEY': SECRET_KEY
+}
+
+DB_FILE = '/Users/apiwat/Documents/FULLSTACK-APP/app.db'
+BARS_URL = 'https://data.alpaca.markets/v2'
+
+DB_HOST = 'localhost' #https://dev.to/balt1794/postgresql-port-5432-already-in-use-1pcf
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'app'
+
+BREAKOUT_SYMBOLS_5MX0_AB = ['MNPR', 'NCLH', 'IONM', 'AFMD', 'STEP', 'AMTX', 'SDS', 'MREO', 'HRZN', 'RWM', 'BTE', 'USM', 'IMTX', 'AGIO', 'SH', 'MRO', 'ARRY', 'OMER']
+
+BREAKOUT_SYMBOLS_5MX0_BB = ['MVIS', 'ORGN', 'GNTX', 'DBX', 'MQ', 'BSM', 'NE', 'MNMD', 'MAG', 'GERN', 'AMRX', 'SIGA', 'CRMD', 'BKR', 'RES', 'AUR', 'HSAI', 'CXAI', 'MODG', 'NN', 'ADV', 'CVE', 'NOG', 'DCGO', 'GM', 'LUNR', 'ADPT', 'BGFV', 'BSBR', 'GEVO', 'SPRY', 'TILE', 'SPXS', 'YETI', 'VTNR', 'NG', 'IMAX', 'AR', 'ALGM', 'LCTX', 'HAL', 'GFI', 'JOBY', 'SPDN', 'FINV', 'ULCC', 'CSTM', 'MAMA', 'STR', 'SH', 'KAR', 'KRG', 'CNX', 'GPS', 'BCSF', 'ACB', 'VIV', 'IVR', 'FUBO', 'CLVT', 'LU', 'EU', 'DK', 'LXRX', 'MGY', 'COTY']
