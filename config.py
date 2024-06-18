8489296580ac4f80ede57c6d0477deb6688ac965
apiwat108 <89001155+apiwat108@users.noreply.github.com>
2024-06-18 17:48:37 +0700

    Update for Jun 18

---
+++ config.py
@@ -0,0 +1,20 @@
+API_KEY = 'PKEQJTZ7QZME52SF9DC8'
+SECRET_KEY = 'OVTOPLtaHdaceplanKmtkrS4xthqQzr4eHooCzlV'
+API_URL = 'https://paper-api.alpaca.markets'
+
+HEADERS = {
+    'APCA-API-KEY-ID': API_KEY,
+    'APCA-API-SECRET-KEY': SECRET_KEY
+}
+
+DB_FILE = '/Users/apiwat/Documents/FULLSTACK-APP/app.db'
+BARS_URL = 'https://data.alpaca.markets/v2'
+
+DB_HOST = 'localhost' #https://dev.to/balt1794/postgresql-port-5432-already-in-use-1pcf
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'app'
+
+BREAKOUT_SYMBOLS_5MX0_AB = ['AUPH', 'BBAR', 'YMM', 'CNK', 'NSA', 'VFS', 'MNKD', 'FPI', 'EVCM', 'GRND', 'RNA', 'CIM', 'NCNO', 'RPAY', 'OCUL', 'REPL', 'PINS', 'IVR', 'URGN', 'ENVX', 'GERN', 'ESI', 'AAN', 'TGS', 'CLBT', 'ARMK', 'PAM', 'SNAP', 'BDTX', 'UE', 'VIRC']
+
+BREAKOUT_SYMBOLS_5MX0_BB = ['ALTM', 'EC', 'M', 'KURA', 'AOSL', 'WVE', 'DNB', 'CDP', 'MNKD', 'IMMR', 'FCPT', 'MODV', 'LU', 'CFB', 'LUNR', 'TPIC', 'DT', 'ACB', 'LAZR', 'ESI', 'RCM', 'TGS', 'SGML', 'AQN', 'CCO', 'AVNT', 'CM', 'PLL', 'PAM', 'GNTX', 'SNAP', 'PAA', 'AR', 'PAGS', 'DESP', 'RUM', 'COMP', 'MAX', 'APAM', 'SBH', 'GPK', 'BRCC', 'QBTS', 'XERS', 'BBAI', 'EPC', 'EVCM', 'RWT', 'IMAX', 'UPWK', 'TNL', 'UROY', 'SPDN', 'RPAY', 'ELAN', 'FULT', 'PSEC', 'PRME', 'TU', 'FHB', 'PAGP', 'BNS', 'UPST', 'SEM', 'RWM', 'IVR', 'PPBI', 'TRTX', 'MITT', 'MAMA', 'MGY', 'DRIP', 'APLD', 'INVH', 'VRRM', 'HAL', 'VIRC', 'PSTX', 'CVE', 'ARCC', 'GDOT', 'WSR', 'AUPH', 'ADTN', 'ALGM', 'LTRX', 'EARN', 'CSTM', 'ET', 'SNDL', 'KN', 'PLRX', 'INFN', 'DUO', 'MAG', 'CLOV', 'DOC', 'MD', 'LSPD', 'SPXS', 'PINS', 'CTRM', 'MTG', 'ENVX', 'ITUB', 'CRBG', 'RSKD', 'XFLT', 'IRBT', 'SWI', 'MYGN', 'CLB', 'ARMK', 'AVPT', 'EQX', 'UUUU', 'IART', 'LADR', 'HSII', 'AMRX', 'DBX', 'IBRX', 'BSAC', 'TLS', 'DX', 'FIBK', 'DK', 'CSWC', 'MANU', 'BBIO', 'FPI', 'AM', 'CPB', 'RKLB', 'TARS', 'EXLS', 'NU', 'OSCR', 'CION', 'UA', 'OPRA', 'MEG', 'WT', 'HE', 'TSEM', 'ASTH', 'BKR', 'VSAT', 'THS', 'MNMD', 'AAT', 'IPG', 'UTG', 'OB']
