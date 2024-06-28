cce42d419050c2540040696b44a44bebe97df3a0
apiwat108 <89001155+apiwat108@users.noreply.github.com>
2024-06-28 18:13:33 +0700

    Update for Jun 28

---
+++ config.py
@@ -0,0 +1,20 @@
+API_KEY = 'PKEQJTZ7QZME52SF9DC8'
+SECRET_KEY = 'OVTOPLtaHdaceplanKmtkrS4xthqQzr4eHooCzlV'
+API_URL = 'https://paper-api.alpaca.markets'
+
+HEADERS = {
+    'APCA-API-KEY-ID': API_KEY,
+    'APCA-API-SECRET-KEY': SECRET_KEY
+}
+
+DB_FILE = '/Users/apiwat/Documents/FULLSTACK-APP/app.db'
+BARS_URL = 'https://data.alpaca.markets/v2'
+
+DB_HOST = 'localhost' #https://dev.to/balt1794/postgresql-port-5432-already-in-use-1pcf
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'app'
+
+BREAKOUT_SYMBOLS_5MX0_AB =  ['ARMK', 'EXLS', 'ENVX', 'REPL', 'URGN', 'HTGC', 'CMRE', 'PACK', 'JOBY', 'NCNO', 'TFC', 'CCL', 'CDP', 'ARWR', 'RXO', 'SYF', 'AAN', 'ZIMV', 'HIMX', 'CLBT', 'SHCR', 'CNK', 'VFS', 'OCUL', 'TVTX', 'TGS', 'LZB', 'TGI', 'RNA', 'BARK', 'ESI', 'FIBK', 'CUK', 'BFH', 'SILK', 'DYN', 'CMTL', 'NRIX', 'GGR', 'ATEX', 'KSS', 'CIM', 'MNKD', 'SNAP', 'GOVX', 'ATER', 'FPI', 'UE', 'EVGO', 'GRND', 'VNT', 'AUPH', 'EVCM', 'ADT', 'NSA', 'BTBT', 'NBTB', 'XPRO', 'IMAX', 'VRRM']
+
+BREAKOUT_SYMBOLS_5MX0_BB = ['MAX', 'MARA', 'GPRK', 'AM', 'ELAN', 'WKC', 'TPIC', 'BNS', 'INFN', 'MANU', 'ODD', 'PSEC', 'AMRX', 'TARS', 'INVH', 'MITT', 'CCO', 'HSII', 'SEM', 'FULT', 'PLRX', 'CLB', 'RYAM', 'MFC', 'CION', 'AR', 'SHOO', 'DRIP', 'WOW', 'RIVN', 'GNTX', 'ET', 'IVR', 'PAA', 'CVE', 'BKR', 'KDP', 'EBS', 'PPBI', 'PAGS', 'BBIO', 'TLS', 'PRME', 'MEG', 'ARKO', 'AAT', 'NCLH', 'GIL', 'KURA', 'BRCC', 'HUBG', 'RUN', 'MTG', 'TSEM', 'HSTM', 'CSWC', 'FL', 'OPRA', 'TELL', 'CALX', 'LUNR', 'HAL', 'MGY', 'RCM', 'LOCO', 'RSKD', 'OXLC', 'KN', 'VSAT', 'COMP', 'SGML', 'UPST', 'XFLT', 'OLMA', 'MNMD', 'DT', 'MAG', 'ACB', 'AVPT', 'AVNT', 'CPB', 'TRP', 'DX', 'IRBT', 'EQX', 'RKLB', 'AOSL', 'ITUB', 'OSCR', 'DBX', 'EPC', 'SPDN', 'UTG', 'PLL', 'CSTM', 'TRTX', 'BBAI', 'TNL', 'VICR', 'IGT', 'M', 'FHB', 'WSR', 'VYGR', 'KGC', 'MAMA', 'UPWK', 'GDOT', 'RWT', 'ASTH', 'HLF', 'LU', 'ZYME', 'LAZR', 'IP', 'EMLP', 'AXGN', 'ALTM', 'GMRE', 'MYGN', 'GPK', 'SBH', 'WOOF', 'UAA', 'RUM', 'TU', 'CGBD', 'PAM', 'LSPD', 'IART', 'PINS', 'BY', 'RWM', 'OGN', 'NR', 'FCPT', 'DNLI', 'WVE', 'SWI', 'ADTN', 'THS', 'SNDL', 'DOC', 'IPG', 'PAGP', 'TGTX', 'IMMR', 'BSAC', 'WT', 'FIP', 'HTLF', 'ARCC', 'APAM', 'DK', 'SPXS', 'CGAU']
