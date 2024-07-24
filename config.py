8124dbbbda05c8fc9865f87466d40402fe8d4f64
apiwat108 <89001155+apiwat108@users.noreply.github.com>
2024-07-24 18:19:56 +0700

    Update for 24 Jul

---
+++ config.py
@@ -0,0 +1,20 @@
+API_KEY = 'AK9WJUKS418ZU6Y8LM66'
+SECRET_KEY = 'JBu0oIEfoUUSuMtu68XglUruZdDBPbzZdAhs8a2m'
+API_URL = 'https://api.alpaca.markets'
+
+HEADERS = {
+    'APCA-API-KEY-ID': API_KEY,
+    'APCA-API-SECRET-KEY': SECRET_KEY
+}
+
+DB_FILE = '/Users/apiwat/Documents/FULLSTACK-APP/app.db'
+BARS_URL = 'https://data.alpaca.markets/v2'
+
+DB_HOST = 'localhost' #https://dev.to/balt1794/postgresql-port-5432-already-in-use-1pcf
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'app'
+
+BREAKOUT_SYMBOLS_5MX0_AB = ['EXK', 'VVV', 'LRMR', 'MATV', 'ESRT', 'FUFU', 'HIMX', 'HEPS', 'EXAI', 'APEI', 'ORC', 'OUT', 'CDP', 'TAC', 'NSA', 'MCW', 'GH', 'AEHR', 'GLW', 'ESI', 'LXRX', 'CIM', 'GLBE', 'NYCB', 'ENVX', 'GBTG', 'PLSE', 'CCL', 'EXLS', 'WLY', 'ERAS', 'PBI', 'ZG', 'CUBE', 'TELL', 'ERJ', 'GPRO', 'VRRM', 'BHVN', 'EOSE', 'ECX', 'IREN', 'ATXS', 'CLBT', 'CNNE', 'TOST', 'LC', 'CUK', 'GRND', 'CMAX', 'ADPT', 'BFH', 'LAUR', 'DOMO', 'ITOS', 'BITF', 'JMIA', 'IONS', 'NVGS', 'JBLU', 'AEHL', 'JOBY', 'HOOD', 'GATO', 'AMH', 'FYBR', 'EVRI', 'AUTL', 'ARIS', 'ONON', 'KNSA', 'INFN', 'PPC', 'MREO', 'TVTX', 'INO', 'MDWD', 'BORR', 'NBTB', 'XERS', 'SYF', 'MNKD', 'NABL', 'SLRN', 'LGVN', 'SDIG', 'DOYU', 'JAMF', 'RVMD', 'MYO', 'RXT', 'JBGS', 'MAG', 'CNK', 'IMXI', 'GLPI', 'KN', 'NRGV', 'AVDL', 'BBDC', 'DOCS', 'WTTR', 'NWSA', 'MRC', 'EVGO', 'BZH', 'MRVI', 'NEM', 'ARLO', 'FHN', 'NBBK', 'ASTH', 'SLN', 'NKTR', 'EWTX', 'KYMR', 'VICI']
+
+# BREAKOUT_SYMBOLS_5MX0_BB = ['MAX', 'MARA', 'GPRK', 'AM', 'ELAN', 'WKC', 'TPIC', 'BNS', 'INFN', 'MANU', 'ODD', 'PSEC', 'AMRX', 'TARS', 'INVH', 'MITT', 'CCO', 'HSII', 'SEM', 'FULT', 'PLRX', 'CLB', 'RYAM', 'MFC', 'CION', 'AR', 'SHOO', 'DRIP', 'WOW', 'RIVN', 'GNTX', 'ET', 'IVR', 'PAA', 'CVE', 'BKR', 'KDP', 'EBS', 'PPBI', 'PAGS', 'BBIO', 'TLS', 'PRME', 'MEG', 'ARKO', 'AAT', 'NCLH', 'GIL', 'KURA', 'BRCC', 'HUBG', 'RUN', 'MTG', 'TSEM', 'HSTM', 'CSWC', 'FL', 'OPRA', 'TELL', 'CALX', 'LUNR', 'HAL', 'MGY', 'RCM', 'LOCO', 'RSKD', 'OXLC', 'KN', 'VSAT', 'COMP', 'SGML', 'UPST', 'XFLT', 'OLMA', 'MNMD', 'DT', 'MAG', 'ACB', 'AVPT', 'AVNT', 'CPB', 'TRP', 'DX', 'IRBT', 'EQX', 'RKLB', 'AOSL', 'ITUB', 'OSCR', 'DBX', 'EPC', 'SPDN', 'UTG', 'PLL', 'CSTM', 'TRTX', 'BBAI', 'TNL', 'VICR', 'IGT', 'M', 'FHB', 'WSR', 'VYGR', 'KGC', 'MAMA', 'UPWK', 'GDOT', 'RWT', 'ASTH', 'HLF', 'LU', 'ZYME', 'LAZR', 'IP', 'EMLP', 'AXGN', 'ALTM', 'GMRE', 'MYGN', 'GPK', 'SBH', 'WOOF', 'UAA', 'RUM', 'TU', 'CGBD', 'PAM', 'LSPD', 'IART', 'PINS', 'BY', 'RWM', 'OGN', 'NR', 'FCPT', 'DNLI', 'WVE', 'SWI', 'ADTN', 'THS', 'SNDL', 'DOC', 'IPG', 'PAGP', 'TGTX', 'IMMR', 'BSAC', 'WT', 'FIP', 'HTLF', 'ARCC', 'APAM', 'DK', 'SPXS', 'CGAU']
