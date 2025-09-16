795f1ed08b8bf3f5ccc65a3e0da526352272ae16
apiwat108 <89001155+apiwat108@users.noreply.github.com>
2025-09-16 22:31:00 +0700

    Update for 16 Sep

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
-# # Live Account
-# API_KEY = 'AK9WJUKS418ZU6Y8LM66'
-# SECRET_KEY = 'JBu0oIEfoUUSuMtu68XglUruZdDBPbzZdAhs8a2m'
-# API_URL = 'https://api.alpaca.markets'
+# Live Account
+API_KEY = 'AK9WJUKS418ZU6Y8LM66'
+SECRET_KEY = 'JBu0oIEfoUUSuMtu68XglUruZdDBPbzZdAhs8a2m'
+API_URL = 'https://api.alpaca.markets'
 
-# Paper Account 1
-API_KEY = 'PKFPXIK488ZVOTTE62E0'
-SECRET_KEY = 'eKA6lLM0CGvfsA3LeoS65bgbX8VkiEdvlaOPvCM0'
-API_URL = 'https://paper-api.alpaca.markets'
+# # Paper Account 1
+# API_KEY = 'PKFPXIK488ZVOTTE62E0'
+# SECRET_KEY = 'eKA6lLM0CGvfsA3LeoS65bgbX8VkiEdvlaOPvCM0'
+# API_URL = 'https://paper-api.alpaca.markets'
 
-# Paper Account 2
-PAPER_ACCOUNT_2 = {
-    "API_KEY": "PKOJ4CR6F799HJ5LYZ8A",
-    "SECRET_KEY": "MWh3Uhy3UWzEaauZUbcRqH15BhHCkqXhrb3hxdOi",
-    "API_URL": "https://paper-api.alpaca.markets"
-}
+# # Paper Account 2
+# PAPER_ACCOUNT_2 = {
+#     "API_KEY": "PKOJ4CR6F799HJ5LYZ8A",
+#     "SECRET_KEY": "MWh3Uhy3UWzEaauZUbcRqH15BhHCkqXhrb3hxdOi",
+#     "API_URL": "https://paper-api.alpaca.markets"
+# }
 
 HEADERS = {
     'APCA-API-KEY-ID': API_KEY,
